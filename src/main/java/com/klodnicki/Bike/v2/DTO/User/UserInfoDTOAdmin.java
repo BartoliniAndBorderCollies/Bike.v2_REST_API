980d6a6201df8595bb399dc4e0457d48c76a248d
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-04 09:44:44 +0200

    add DTO about User for Admin

---
+++ src/main/java/com/klodnicki/Bike/v2/DTO/User/UserInfoDTOAdmin.java
@@ -0,0 +1,68 @@
+package com.klodnicki.Bike.v2.DTO.User;
+
+public class UserInfoDTOAdmin {
+
+    private String name;
+    private String phoneNumber;
+    private String emailAddress;
+    private int accountNumber;
+    private boolean isAccountValid;
+    private String role;
+
+    public UserInfoDTOAdmin(String name, String phoneNumber, String emailAddress, int accountNumber, boolean isAccountValid, String role) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+        this.accountNumber = accountNumber;
+        this.isAccountValid = isAccountValid;
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public boolean isAccountValid() {
+        return isAccountValid;
+    }
+
+    public void setAccountValid(boolean accountValid) {
+        isAccountValid = accountValid;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
