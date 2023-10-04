89583ac9f0d566fc51f284fe885fe19c7c67052a
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-04 09:44:28 +0200

    add DTO about Bike for normal User

---
+++ src/main/java/com/klodnicki/Bike/v2/DTO/Bike/BikeInfoDTONormalUser.java
@@ -0,0 +1,42 @@
+package com.klodnicki.Bike.v2.DTO.Bike;
+
+import com.klodnicki.Bike.v2.model.BikeType;
+
+public class BikeInfoDTONormalUser {
+
+    private String serialNumber;
+
+    private boolean isRented;
+
+    private BikeType bikeType;
+
+    public BikeInfoDTONormalUser(String serialNumber, boolean isRented, BikeType bikeType) {
+        this.serialNumber = serialNumber;
+        this.isRented = isRented;
+        this.bikeType = bikeType;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public boolean isRented() {
+        return isRented;
+    }
+
+    public void setRented(boolean rented) {
+        isRented = rented;
+    }
+
+    public BikeType getBikeType() {
+        return bikeType;
+    }
+
+    public void setBikeType(BikeType bikeType) {
+        this.bikeType = bikeType;
+    }
+}
