d99839b84a3345a31510f4cd83517e0dbefc4bfa
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-04 19:13:00 +0200

    add additional fields to ChargingStation

--- src/main/java/com/klodnicki/Bike/v2/model/entity/ChargingStation.java
+++ src/main/java/com/klodnicki/Bike/v2/model/entity/ChargingStation.java
@@ -10,12 +10,15 @@ public class ChargingStation {
     private Long id;
 
     private String name;
-
+    private String address;
+    private String city;
     private int freeSlots;
 
-    public ChargingStation(Long id, String name, int freeSlots) {
+    public ChargingStation(Long id, String name, String address, String city, int freeSlots) {
         this.id = id;
         this.name = name;
+        this.address = address;
+        this.city = city;
         this.freeSlots = freeSlots;
     }
 
@@ -44,5 +47,21 @@ public class ChargingStation {
 
     public void setFreeSlots(int freeSlots) {
         this.freeSlots = freeSlots;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
     }
 }
