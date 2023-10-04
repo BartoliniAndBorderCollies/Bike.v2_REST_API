977cb45023228fbd530232337825f856e389509c
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-04 19:12:40 +0200

    add additional fields to Bike

--- src/main/java/com/klodnicki/Bike/v2/model/entity/Bike.java
+++ src/main/java/com/klodnicki/Bike/v2/model/entity/Bike.java
@@ -3,6 +3,8 @@ package com.klodnicki.Bike.v2.model.entity;
 import com.klodnicki.Bike.v2.model.BikeType;
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 public class Bike {
     @Id
@@ -11,16 +13,23 @@ public class Bike {
     private Long id;
 
     private String serialNumber;
-
     private boolean isRented;
-
     private BikeType bikeType;
+    private LocalDateTime rentalStartTime;
+    private LocalDateTime rentalEndTime;
+    private double amountToBePaid;
+    private String gpsCoordinates;
 
-    public Bike(Long id, String serialNumber, boolean isRented, BikeType bikeType) {
+    public Bike(Long id, String serialNumber, boolean isRented, BikeType bikeType, LocalDateTime rentalStartTime,
+                LocalDateTime rentalEndTime, double amountToBePaid, String gpsCoordinates) {
         this.id = id;
         this.serialNumber = serialNumber;
         this.isRented = isRented;
         this.bikeType = bikeType;
+        this.rentalStartTime = rentalStartTime;
+        this.rentalEndTime = rentalEndTime;
+        this.amountToBePaid = amountToBePaid;
+        this.gpsCoordinates = gpsCoordinates;
     }
 
     public Bike() {
@@ -56,5 +65,37 @@ public class Bike {
 
     public void setBikeType(BikeType bikeType) {
         this.bikeType = bikeType;
+    }
+
+    public LocalDateTime getRentalStartTime() {
+        return rentalStartTime;
+    }
+
+    public void setRentalStartTime(LocalDateTime rentalStartTime) {
+        this.rentalStartTime = rentalStartTime;
+    }
+
+    public LocalDateTime getRentalEndTime() {
+        return rentalEndTime;
+    }
+
+    public void setRentalEndTime(LocalDateTime rentalEndTime) {
+        this.rentalEndTime = rentalEndTime;
+    }
+
+    public double getAmountToBePaid() {
+        return amountToBePaid;
+    }
+
+    public void setAmountToBePaid(double amountToBePaid) {
+        this.amountToBePaid = amountToBePaid;
+    }
+
+    public String getGpsCoordinates() {
+        return gpsCoordinates;
+    }
+
+    public void setGpsCoordinates(String gpsCoordinates) {
+        this.gpsCoordinates = gpsCoordinates;
     }
 }
