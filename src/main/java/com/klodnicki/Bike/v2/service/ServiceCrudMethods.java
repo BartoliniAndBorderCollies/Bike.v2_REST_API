870280ed6c597051d7494aa76bb43b2550ed938f
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-06 13:37:12 +0200

    add generic interface with CRUD methods

---
+++ src/main/java/com/klodnicki/Bike/v2/service/ServiceCrudMethods.java
@@ -0,0 +1,15 @@
+package com.klodnicki.Bike.v2.service;
+
+public interface ServiceCrudMethods <T> {
+
+    <T> T add();
+
+    <T> T findById(Long id);
+
+    <T> Iterable<T> findAll();
+
+    <T> void deleteById(Long id);
+
+    <T> T update(Long id);
+
+}
