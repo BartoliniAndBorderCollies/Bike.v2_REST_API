4e0358f1d71e421c45ff4492665f1e3b5ba6085f
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-06 11:32:54 +0200

    update of Bike considering abstract class

--- src/main/java/com/klodnicki/Bike/v2/model/entity/Bike.java
+++ src/main/java/com/klodnicki/Bike/v2/model/entity/Bike.java
@@ -1,37 +1,25 @@
 package com.klodnicki.Bike.v2.model.entity;
 
 import com.klodnicki.Bike.v2.model.BikeType;
-import com.klodnicki.Bike.v2.model.Vehicle;
-import com.klodnicki.Bike.v2.model.RentalAction;
+import com.klodnicki.Bike.v2.model.RentableVehicle;
+import com.klodnicki.Bike.v2.model.VehicleRentalAction;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 
 @Entity
-public class Bike extends Vehicle implements RentalAction {
+public class Bike extends RentableVehicle implements VehicleRentalAction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
-
-    private String serialNumber;
-    private boolean isRented;
     private BikeType bikeType;
-    private LocalDateTime rentalStartTime;
-    private LocalDateTime rentalEndTime;
-    private double amountToBePaid;
-    private String gpsCoordinates;
 
-    public Bike(Long id, String serialNumber, boolean isRented, BikeType bikeType, LocalDateTime rentalStartTime,
+    public Bike(Long id, BikeType bikeType, String serialNumber, boolean isRented, LocalDateTime rentalStartTime,
                 LocalDateTime rentalEndTime, double amountToBePaid, String gpsCoordinates) {
+        super(serialNumber, isRented, rentalStartTime, rentalEndTime, amountToBePaid, gpsCoordinates);
         this.id = id;
-        this.serialNumber = serialNumber;
-        this.isRented = isRented;
         this.bikeType = bikeType;
-        this.rentalStartTime = rentalStartTime;
-        this.rentalEndTime = rentalEndTime;
-        this.amountToBePaid = amountToBePaid;
-        this.gpsCoordinates = gpsCoordinates;
     }
 
     public Bike() {
@@ -45,60 +33,12 @@ public class Bike extends Vehicle implements RentalAction {
         this.id = id;
     }
 
-    public String getSerialNumber() {
-        return serialNumber;
-    }
-
-    public void setSerialNumber(String serialNumber) {
-        this.serialNumber = serialNumber;
-    }
-
-    public boolean isRented() {
-        return isRented;
-    }
-
-    public void setRented(boolean rented) {
-        isRented = rented;
-    }
-
     public BikeType getBikeType() {
         return bikeType;
     }
 
     public void setBikeType(BikeType bikeType) {
         this.bikeType = bikeType;
-    }
-
-    public LocalDateTime getRentalStartTime() {
-        return rentalStartTime;
-    }
-
-    public void setRentalStartTime(LocalDateTime rentalStartTime) {
-        this.rentalStartTime = rentalStartTime;
-    }
-
-    public LocalDateTime getRentalEndTime() {
-        return rentalEndTime;
-    }
-
-    public void setRentalEndTime(LocalDateTime rentalEndTime) {
-        this.rentalEndTime = rentalEndTime;
-    }
-
-    public double getAmountToBePaid() {
-        return amountToBePaid;
-    }
-
-    public void setAmountToBePaid(double amountToBePaid) {
-        this.amountToBePaid = amountToBePaid;
-    }
-
-    public String getGpsCoordinates() {
-        return gpsCoordinates;
-    }
-
-    public void setGpsCoordinates(String gpsCoordinates) {
-        this.gpsCoordinates = gpsCoordinates;
     }
 
     @Override
