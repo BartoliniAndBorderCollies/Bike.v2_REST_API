99c8c3d30e82611cf8a056787a3d60eeaec737ad
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-06 09:08:40 +0200

    add Bike request DTO

---
+++ src/main/java/com/klodnicki/Bike/v2/DTO/bike/BikeRequestDTO.java
@@ -0,0 +1,94 @@
+package com.klodnicki.Bike.v2.DTO.bike;
+
+import com.klodnicki.Bike.v2.model.BikeType;
+
+import java.time.LocalDateTime;
+
+public class BikeRequestDTO {
+
+    private Long id;
+    private String serialNumber;
+    private boolean isRented;
+    private BikeType bikeType;
+    private LocalDateTime rentalStartTime;
+    private LocalDateTime rentalEndTime;
+    private double amountToBePaid;
+    private String gpsCoordinates;
+
+    public BikeRequestDTO(Long id, String serialNumber, boolean isRented, BikeType bikeType,
+                          LocalDateTime rentalStartTime, LocalDateTime rentalEndTime, double amountToBePaid,
+                          String gpsCoordinates) {
+        this.id = id;
+        this.serialNumber = serialNumber;
+        this.isRented = isRented;
+        this.bikeType = bikeType;
+        this.rentalStartTime = rentalStartTime;
+        this.rentalEndTime = rentalEndTime;
+        this.amountToBePaid = amountToBePaid;
+        this.gpsCoordinates = gpsCoordinates;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public boolean isRented() {
+        return isRented;
+    }
+
+    public void setRented(boolean rented) {
+        isRented = rented;
+    }
+
+    public BikeType getBikeType() {
+        return bikeType;
+    }
+
+    public void setBikeType(BikeType bikeType) {
+        this.bikeType = bikeType;
+    }
+
+    public LocalDateTime getRentalStartTime() {
+        return rentalStartTime;
+    }
+
+    public void setRentalStartTime(LocalDateTime rentalStartTime) {
+        this.rentalStartTime = rentalStartTime;
+    }
+
+    public LocalDateTime getRentalEndTime() {
+        return rentalEndTime;
+    }
+
+    public void setRentalEndTime(LocalDateTime rentalEndTime) {
+        this.rentalEndTime = rentalEndTime;
+    }
+
+    public double getAmountToBePaid() {
+        return amountToBePaid;
+    }
+
+    public void setAmountToBePaid(double amountToBePaid) {
+        this.amountToBePaid = amountToBePaid;
+    }
+
+    public String getGpsCoordinates() {
+        return gpsCoordinates;
+    }
+
+    public void setGpsCoordinates(String gpsCoordinates) {
+        this.gpsCoordinates = gpsCoordinates;
+    }
+}
