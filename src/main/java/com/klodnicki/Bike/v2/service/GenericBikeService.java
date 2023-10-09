d9812dd3dbf5a577b4b82430b509a3f98f5c0c38
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-09 18:11:01 +0200

    add interface GenericBikeService

---
+++ src/main/java/com/klodnicki/Bike/v2/service/GenericBikeService.java
@@ -0,0 +1,7 @@
+package com.klodnicki.Bike.v2.service;
+
+import com.klodnicki.Bike.v2.DTO.bike.BikeForAdminResponseDTO;
+import com.klodnicki.Bike.v2.DTO.bike.BikeRequestDTO;
+
+public interface GenericBikeService extends GenericService<BikeForAdminResponseDTO, BikeRequestDTO> {
+}
