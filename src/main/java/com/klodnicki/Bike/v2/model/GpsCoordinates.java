cc780c884cf6fa968fa557cd6f2abdd3df02d292
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-10 12:27:59 +0200

    add GpsCoordinates class

---
+++ src/main/java/com/klodnicki/Bike/v2/model/GpsCoordinates.java
@@ -0,0 +1,28 @@
+package com.klodnicki.Bike.v2.model;
+
+public class GpsCoordinates {
+
+    private String longitude;
+    private String latitude;
+
+    public GpsCoordinates(String longitude, String latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+}
