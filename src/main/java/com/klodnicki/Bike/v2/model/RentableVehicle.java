7ccad0f75fae4d163d02c9b0e4deb185ecf0b7f6
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-11 16:00:20 +0200

    refactor from protected into private

--- src/main/java/com/klodnicki/Bike/v2/model/RentableVehicle.java
+++ src/main/java/com/klodnicki/Bike/v2/model/RentableVehicle.java
@@ -1,20 +1,26 @@
 package com.klodnicki.Bike.v2.model;
 
-import jakarta.persistence.Embedded;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 
-@MappedSuperclass
+@MappedSuperclass // ta klasa nie może być jednocześnie encją
+//@Entity
+//@Inheritance(strategy = InheritanceType.SINGLE_TABLE) - te dwie adnotacje zrobiłyby, że jest jedna tabela ze wszystkimi
+// kolumnami
+//@DiscriminatorColumn(name="vehicle_type", discriminatorType = DiscriminatorType.STRING)
+
+//@Entity
+//@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class RentableVehicle {
 
-    protected String serialNumber;
-    protected boolean isRented;
-    protected LocalDateTime rentalStartTime;
-    protected LocalDateTime rentalEndTime;
-    protected double amountToBePaid;
+    private String serialNumber;
+    private boolean isRented;
+    private LocalDateTime rentalStartTime;
+    private LocalDateTime rentalEndTime;
+    private double amountToBePaid;
     @Embedded
-    protected GpsCoordinates gpsCoordinates;
+    private GpsCoordinates gpsCoordinates;
 
     public RentableVehicle(String serialNumber, boolean isRented, LocalDateTime rentalStartTime,
                            LocalDateTime rentalEndTime, double amountToBePaid, GpsCoordinates gpsCoordinates) {
