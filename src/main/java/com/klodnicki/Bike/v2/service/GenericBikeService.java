1919641dfce2b5c49d7a1f977508e5472873c864
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-12 19:44:51 +0200

    delete of comment

--- src/main/java/com/klodnicki/Bike/v2/service/GenericBikeService.java
+++ src/main/java/com/klodnicki/Bike/v2/service/GenericBikeService.java
@@ -4,8 +4,4 @@ import com.klodnicki.Bike.v2.DTO.bike.BikeForAdminResponseDTO;
 import com.klodnicki.Bike.v2.DTO.bike.BikeRequestDTO;
 
 public interface GenericBikeService extends GenericService<BikeForAdminResponseDTO, BikeRequestDTO> {
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> master
