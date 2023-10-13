9d5789c9bae50a208b1b6e51f49cc18afccba0cf
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-13 21:00:17 +0200

    add find all available bikes in GenericBikeService

--- src/main/java/com/klodnicki/Bike/v2/service/GenericBikeService.java
+++ src/main/java/com/klodnicki/Bike/v2/service/GenericBikeService.java
@@ -1,7 +1,11 @@
 package com.klodnicki.Bike.v2.service;
 
 import com.klodnicki.Bike.v2.DTO.bike.BikeForAdminResponseDTO;
+import com.klodnicki.Bike.v2.DTO.bike.BikeForNormalUserResponseDTO;
 import com.klodnicki.Bike.v2.DTO.bike.BikeRequestDTO;
 
+import java.util.List;
+
 public interface GenericBikeService extends GenericService<BikeForAdminResponseDTO, BikeRequestDTO> {
+    List<BikeForNormalUserResponseDTO> findAvailableBikes();
 }
