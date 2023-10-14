bc29d4843563788bd9e0cd83cb2381a1bf56ce52
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-14 09:19:33 +0200

    add finding a bike by id and returning BikeForNormalUserResponseDTO

--- src/main/java/com/klodnicki/Bike/v2/service/GenericBikeService.java
+++ src/main/java/com/klodnicki/Bike/v2/service/GenericBikeService.java
@@ -8,4 +8,6 @@ import java.util.List;
 
 public interface GenericBikeService extends GenericService<BikeForAdminResponseDTO, BikeRequestDTO> {
     List<BikeForNormalUserResponseDTO> findAvailableBikes();
+
+    BikeForNormalUserResponseDTO findBikeForNormalUserById(Long id);
 }
