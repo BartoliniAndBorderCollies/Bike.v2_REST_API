bd60a379fefb304723038d44d82eb9474065d2a1
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-15 16:12:06 +0200

    refactor of rentBike in controller to have one @RequestBody

--- src/main/java/com/klodnicki/Bike/v2/rest/controller/RentBikeController.java
+++ src/main/java/com/klodnicki/Bike/v2/rest/controller/RentBikeController.java
@@ -1,6 +1,7 @@
 package com.klodnicki.Bike.v2.rest.controller;
 
 import com.klodnicki.Bike.v2.DTO.bike.BikeForNormalUserResponseDTO;
+import com.klodnicki.Bike.v2.model.RentRequest;
 import com.klodnicki.Bike.v2.model.entity.Bike;
 import com.klodnicki.Bike.v2.model.entity.ChargingStation;
 import com.klodnicki.Bike.v2.model.entity.Rent;
@@ -32,8 +33,8 @@ public class RentBikeController {
     }
 
     @PostMapping("/rentals/add")
-    public Rent rentBike(User user, Bike bike, ChargingStation chargingStation) {
-       return rentBikeService.rentBike(user, bike, chargingStation);
+    public Rent rentBike(@RequestBody RentRequest rentRequest) {
+        return rentBikeService.rentBike(rentRequest.getUser(), rentRequest.getBike(), rentRequest.getChargingStation());
     }
 
 
