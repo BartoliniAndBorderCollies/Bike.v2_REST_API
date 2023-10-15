8fac5df74f3bd0c2bcd0d1b2611e58e51005237f
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-15 08:47:26 +0200

    add renting bike method to RentBikeGenericService

--- src/main/java/com/klodnicki/Bike/v2/service/RentBikeGenericService.java
+++ src/main/java/com/klodnicki/Bike/v2/service/RentBikeGenericService.java
@@ -3,6 +3,10 @@ package com.klodnicki.Bike.v2.service;
 import com.klodnicki.Bike.v2.DTO.bike.BikeForAdminResponseDTO;
 import com.klodnicki.Bike.v2.DTO.bike.BikeForNormalUserResponseDTO;
 import com.klodnicki.Bike.v2.DTO.bike.BikeRequestDTO;
+import com.klodnicki.Bike.v2.model.entity.Bike;
+import com.klodnicki.Bike.v2.model.entity.ChargingStation;
+import com.klodnicki.Bike.v2.model.entity.Rent;
+import com.klodnicki.Bike.v2.model.entity.User;
 
 import java.util.List;
 
@@ -12,4 +16,5 @@ public interface RentBikeGenericService extends GenericService<BikeForNormalUser
 
     BikeForNormalUserResponseDTO findBikeForNormalUserById(Long id);
 
+    Rent rentBike(User user, Bike bike, ChargingStation chargingStation);
 }
