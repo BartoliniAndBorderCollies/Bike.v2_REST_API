98e2451e7781b5c60f5ca6ab606b5a118b6bdc45
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-15 16:12:27 +0200

    delete of TODOs

--- src/main/java/com/klodnicki/Bike/v2/model/entity/Rent.java
+++ src/main/java/com/klodnicki/Bike/v2/model/entity/Rent.java
@@ -9,15 +9,15 @@ public class Rent {
     @Column(name = "id", nullable = false)
     private Long id;
 //Rent class is a child class - or no-owning side - therefore it contains FK, therefore it joins columns
-    @OneToOne(cascade = CascadeType.ALL) //TODO: not sure about this
+    @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "bike_id")
     private Bike bike;
 
-    @OneToOne(cascade = CascadeType.ALL) //TODO: not sure about this
+    @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id")
     private User user;
 
-    @OneToOne(cascade = CascadeType.ALL) //TODO: not sure about this
+    @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "chargingStation_id")
     private ChargingStation chargingStation;
 
