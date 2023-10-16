016295d8e0db0e0bac0891b74bf0bb0c78531cd2
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-16 09:45:44 +0200

    add ChargingStationService with methods

--- src/main/java/com/klodnicki/Bike/v2/service/ChargingStationService.java
+++ src/main/java/com/klodnicki/Bike/v2/service/ChargingStationService.java
@@ -1,7 +1,25 @@
 package com.klodnicki.Bike.v2.service;
 
+import com.klodnicki.Bike.v2.model.entity.ChargingStation;
+import com.klodnicki.Bike.v2.repository.ChargingStationRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ChargingStationService {
+
+    private final ChargingStationRepository chargingStationRepository;
+
+    public ChargingStationService(ChargingStationRepository chargingStationRepository) {
+        this.chargingStationRepository = chargingStationRepository;
+    }
+
+    public ChargingStation add(ChargingStation chargingStation) {
+        return chargingStationRepository.save(chargingStation);
+    }
+
+    public Iterable<ChargingStation> findAll() {
+        return chargingStationRepository.findAll();
+    }
 }
