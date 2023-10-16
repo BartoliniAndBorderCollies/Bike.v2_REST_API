20469c086a3edf946333fceb99757891f3be6f5a
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-16 09:56:59 +0200

    add methods in UserController

--- src/main/java/com/klodnicki/Bike/v2/rest/controller/UserController.java
+++ src/main/java/com/klodnicki/Bike/v2/rest/controller/UserController.java
@@ -1,7 +1,26 @@
 package com.klodnicki.Bike.v2.rest.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.klodnicki.Bike.v2.model.entity.User;
+import com.klodnicki.Bike.v2.service.UserService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/add")
+    public User add(@RequestBody User user) {
+        return userService.add(user);
+    }
+
+    @GetMapping
+    public Iterable<User> findAll () {
+        return userService.findAll();
+    }
 }
