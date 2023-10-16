41f07370dfd0e443736e9308b5a99985c92c08a0
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-16 09:57:11 +0200

    add methods in UserService

--- src/main/java/com/klodnicki/Bike/v2/service/UserService.java
+++ src/main/java/com/klodnicki/Bike/v2/service/UserService.java
@@ -1,7 +1,23 @@
 package com.klodnicki.Bike.v2.service;
 
+import com.klodnicki.Bike.v2.model.entity.User;
+import com.klodnicki.Bike.v2.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User add(User user) {
+        return userRepository.save(user);
+    }
+
+    public Iterable<User> findAll() {
+        return userRepository.findAll();
+    }
 }
