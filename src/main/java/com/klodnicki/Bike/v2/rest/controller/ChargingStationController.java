d3ae88dd0aef94d526e0ddc023353c1483f79314
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-16 09:44:46 +0200

    add ChargingStationController with add()

--- src/main/java/com/klodnicki/Bike/v2/rest/controller/ChargingStationController.java
+++ src/main/java/com/klodnicki/Bike/v2/rest/controller/ChargingStationController.java
@@ -1,7 +1,24 @@
 package com.klodnicki.Bike.v2.rest.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.klodnicki.Bike.v2.model.entity.ChargingStation;
+import com.klodnicki.Bike.v2.service.ChargingStationService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/station")
 public class ChargingStationController {
+
+    private final ChargingStationService chargingStationService;
+
+    public ChargingStationController(ChargingStationService chargingStationService) {
+        this.chargingStationService = chargingStationService;
+    }
+
+    @PostMapping("/add")
+    public ChargingStation add(@RequestBody ChargingStation chargingStation) {
+        return chargingStationService.add(chargingStation);
+    }
+
 }
