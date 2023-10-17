2dd30c01d707d97940708d53608ae0071072606e
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-17 10:08:10 +0200

    use lombok instead of each getter and setter

--- src/main/java/com/klodnicki/Bike/v2/model/RentRequest.java
+++ src/main/java/com/klodnicki/Bike/v2/model/RentRequest.java
@@ -3,35 +3,14 @@ package com.klodnicki.Bike.v2.model;
 import com.klodnicki.Bike.v2.model.entity.Bike;
 import com.klodnicki.Bike.v2.model.entity.ChargingStation;
 import com.klodnicki.Bike.v2.model.entity.User;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class RentRequest {
 
     private Long userId;
     private Long bikeId;
-
     private Long chargingStationId;
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public Long getBikeId() {
-        return bikeId;
-    }
-
-    public void setBikeId(Long bikeId) {
-        this.bikeId = bikeId;
-    }
-
-    public Long getChargingStationId() {
-        return chargingStationId;
-    }
-
-    public void setChargingStationId(Long chargingStationId) {
-        this.chargingStationId = chargingStationId;
-    }
 }
