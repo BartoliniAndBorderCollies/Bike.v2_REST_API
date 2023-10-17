9b3f59bfab3dff402859b02fd8bfd12af3a7b28b
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-17 09:19:20 +0200

    add getBike() to GenericBikeService

--- src/main/java/com/klodnicki/Bike/v2/service/GenericBikeService.java
+++ src/main/java/com/klodnicki/Bike/v2/service/GenericBikeService.java
@@ -3,9 +3,11 @@ package com.klodnicki.Bike.v2.service;
 import com.klodnicki.Bike.v2.DTO.bike.BikeForAdminResponseDTO;
 import com.klodnicki.Bike.v2.DTO.bike.BikeForNormalUserResponseDTO;
 import com.klodnicki.Bike.v2.DTO.bike.BikeRequestDTO;
+import com.klodnicki.Bike.v2.model.entity.Bike;
 
 import java.util.List;
 
 public interface GenericBikeService extends GenericService<BikeForAdminResponseDTO, BikeRequestDTO> {
 
+    Bike getBike(Long id);
 }
