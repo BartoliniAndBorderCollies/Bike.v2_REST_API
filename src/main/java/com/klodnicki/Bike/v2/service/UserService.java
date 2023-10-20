4674fd9b4bf1a51a6c6fecc0ffffbdbdb0f67b91
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-20 11:11:09 +0200

    remake of findAll() -> returning List of DTOs for Admin in UserService

--- src/main/java/com/klodnicki/Bike/v2/service/UserService.java
+++ src/main/java/com/klodnicki/Bike/v2/service/UserService.java
@@ -1,13 +1,20 @@
 package com.klodnicki.Bike.v2.service;
 
+import com.klodnicki.Bike.v2.DTO.user.UserForAdminResponseDTO;
 import com.klodnicki.Bike.v2.model.entity.User;
 import com.klodnicki.Bike.v2.repository.UserRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+
+    ModelMapper modelMapper = new ModelMapper();
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -17,8 +24,15 @@ public class UserService {
         return userRepository.save(user);
     }
 
-    public Iterable<User> findAll() {
-        return userRepository.findAll();
+    public List<UserForAdminResponseDTO> findAll() {
+        Iterable<User> users = userRepository.findAll();
+        List<UserForAdminResponseDTO> listUsersDTO = new ArrayList<>();
+
+        for (User user: users) {
+            UserForAdminResponseDTO userDto = modelMapper.map(user, UserForAdminResponseDTO.class);
+            listUsersDTO.add(userDto);
+        }
+        return listUsersDTO;
     }
 
     public User findById(Long id) {
