2c9755c7bd58b1555726b122dd8013575e5044b0
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-20 11:10:07 +0200

    add id field, @Getter,@Setter and @AllArgsConstructor to UserForAdminResponseDTO

--- src/main/java/com/klodnicki/Bike/v2/DTO/user/UserForAdminResponseDTO.java
+++ src/main/java/com/klodnicki/Bike/v2/DTO/user/UserForAdminResponseDTO.java
@@ -1,7 +1,15 @@
 package com.klodnicki.Bike.v2.DTO.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class UserForAdminResponseDTO {
 
+    private Long id;
     private String name;
     private String phoneNumber;
     private String emailAddress;
@@ -10,70 +18,6 @@ public class UserForAdminResponseDTO {
     private String role;
     private double balance;
 
-    public UserForAdminResponseDTO(String name, String phoneNumber, String emailAddress, int accountNumber,
-                                   boolean isAccountValid, String role, double balance) {
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-        this.emailAddress = emailAddress;
-        this.accountNumber = accountNumber;
-        this.isAccountValid = isAccountValid;
-        this.role = role;
-        this.balance = balance;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmailAddress() {
-        return emailAddress;
-    }
-
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
-
-    public int getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(int accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public boolean isAccountValid() {
-        return isAccountValid;
-    }
-
-    public void setAccountValid(boolean accountValid) {
-        isAccountValid = accountValid;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
+    public UserForAdminResponseDTO() {
     }
 }
