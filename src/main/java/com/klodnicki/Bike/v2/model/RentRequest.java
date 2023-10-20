7f6ec69ca3091e47a4b1c7bb84f573fdbd98ecc5
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-20 09:01:13 +0200

    delete of unused imports

--- src/main/java/com/klodnicki/Bike/v2/model/RentRequest.java
+++ src/main/java/com/klodnicki/Bike/v2/model/RentRequest.java
@@ -1,8 +1,5 @@
 package com.klodnicki.Bike.v2.model;
 
-import com.klodnicki.Bike.v2.model.entity.Bike;
-import com.klodnicki.Bike.v2.model.entity.ChargingStation;
-import com.klodnicki.Bike.v2.model.entity.User;
 import lombok.Getter;
 import lombok.Setter;
 
