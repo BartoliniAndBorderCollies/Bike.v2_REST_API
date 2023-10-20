253654feacf60a1269030f7bef880a0ec1b52354
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-20 12:23:29 +0200

    add Feedback entity class

---
+++ src/main/java/com/klodnicki/Bike/v2/model/entity/Feedback.java
@@ -0,0 +1,21 @@
+package com.klodnicki.Bike.v2.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Feedback {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String comment;
+}
