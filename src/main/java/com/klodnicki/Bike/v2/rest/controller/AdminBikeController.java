1ce9ddde388f1924b5454631fdd87670dab0605b
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-21 11:24:11 +0200

    rename AdminController to AdminBikeController

--- src/main/java/com/klodnicki/Bike/v2/rest/controller/AdminBikeController.java
+++ src/main/java/com/klodnicki/Bike/v2/rest/controller/AdminBikeController.java
@@ -2,23 +2,21 @@ package com.klodnicki.Bike.v2.rest.controller;
 
 import com.klodnicki.Bike.v2.DTO.bike.BikeForAdminResponseDTO;
 import com.klodnicki.Bike.v2.DTO.bike.BikeRequestDTO;
-import com.klodnicki.Bike.v2.DTO.user.UserForAdminResponseDTO;
 import com.klodnicki.Bike.v2.service.GenericBikeService;
 import com.klodnicki.Bike.v2.service.UserService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
-public class AdminController {
+public class AdminBikeController {
 
     private final GenericBikeService bikeService;
 
     private final UserService userService;
 
-    public AdminController(GenericBikeService bikeService, UserService userService) {
+    public AdminBikeController(GenericBikeService bikeService, UserService userService) {
         this.bikeService = bikeService;
         this.userService = userService;
     }
