70979e46cd289c9c502c3d0df5555a712fbf576d
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-22 10:05:45 +0200

    add AdminChargingStationController with basic methods

---
+++ src/main/java/com/klodnicki/Bike/v2/rest/controller/AdminChargingStationController.java
@@ -0,0 +1,32 @@
+package com.klodnicki.Bike.v2.rest.controller;
+
+import com.klodnicki.Bike.v2.DTO.station.StationForAdminResponseDTO;
+import com.klodnicki.Bike.v2.model.entity.ChargingStation;
+import com.klodnicki.Bike.v2.service.ChargingStationService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/admin/stations")
+public class AdminChargingStationController {
+
+    private final ChargingStationService chargingStationService;
+
+    public AdminChargingStationController(ChargingStationService chargingStationService) {
+        this.chargingStationService = chargingStationService;
+    }
+
+    @PostMapping("/add")
+    public StationForAdminResponseDTO add(@RequestBody ChargingStation chargingStation) {
+        return chargingStationService.add(chargingStation);
+    }
+
+    @GetMapping("/{id}")
+    public ChargingStation findById(@PathVariable("id") Long id) {
+        return chargingStationService.findById(id);
+    }
+
+    @GetMapping
+    public Iterable<ChargingStation> findAll() {
+        return chargingStationService.findAll();
+    }
+}
