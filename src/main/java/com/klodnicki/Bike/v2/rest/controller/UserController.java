35728d407c5e79ee5dfdcc6976fed1708c3ce7e8
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-22 12:18:07 +0200

    refactor of add() to return UserForAdminResponseDTO while adding a user

--- src/main/java/com/klodnicki/Bike/v2/rest/controller/UserController.java
+++ src/main/java/com/klodnicki/Bike/v2/rest/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.klodnicki.Bike.v2.rest.controller;
 
+import com.klodnicki.Bike.v2.DTO.user.UserForAdminResponseDTO;
 import com.klodnicki.Bike.v2.DTO.user.UserForNormalUserResponseDTO;
 import com.klodnicki.Bike.v2.model.entity.User;
 import com.klodnicki.Bike.v2.service.UserService;
@@ -16,7 +17,7 @@ public class UserController {
     }
 
     @PostMapping("/add")
-    public UserForNormalUserResponseDTO add(@RequestBody User user) {
+    public UserForAdminResponseDTO add(@RequestBody User user) {
         return userService.add(user);
     }
 
