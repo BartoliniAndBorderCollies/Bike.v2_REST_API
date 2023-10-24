9662abb712ad3ca628bf4508ef4b8257f572a8fe
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-24 12:38:37 +0200

    add lombok annotations for rent request DTOs

--- src/main/java/com/klodnicki/Bike/v2/DTO/rent/RentRequestDTO.java
+++ src/main/java/com/klodnicki/Bike/v2/DTO/rent/RentRequestDTO.java
@@ -3,9 +3,13 @@ package com.klodnicki.Bike.v2.DTO.rent;
 import com.klodnicki.Bike.v2.DTO.bike.BikeForNormalUserResponseDTO;
 import com.klodnicki.Bike.v2.DTO.station.StationForNormalUserResponseDTO;
 import com.klodnicki.Bike.v2.DTO.user.UserForNormalUserResponseDTO;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 public class RentRequestDTO {
 
     private Long id;
@@ -15,60 +19,4 @@ public class RentRequestDTO {
     private BikeForNormalUserResponseDTO bikeForNormalUserResponseDTO;
     private UserForNormalUserResponseDTO userForNormalUserResponseDTO;
     private StationForNormalUserResponseDTO stationForNormalUserResponseDTO;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getRentalStartTime() {
-        return rentalStartTime;
-    }
-
-    public void setRentalStartTime(LocalDateTime rentalStartTime) {
-        this.rentalStartTime = rentalStartTime;
-    }
-
-    public LocalDateTime getRentalEndTime() {
-        return rentalEndTime;
-    }
-
-    public void setRentalEndTime(LocalDateTime rentalEndTime) {
-        this.rentalEndTime = rentalEndTime;
-    }
-
-    public int getDaysOfRent() {
-        return daysOfRent;
-    }
-
-    public void setDaysOfRent(int daysOfRent) {
-        this.daysOfRent = daysOfRent;
-    }
-
-    public BikeForNormalUserResponseDTO getBikeForNormalUserResponseDTO() {
-        return bikeForNormalUserResponseDTO;
-    }
-
-    public void setBikeForNormalUserResponseDTO(BikeForNormalUserResponseDTO bikeForNormalUserResponseDTO) {
-        this.bikeForNormalUserResponseDTO = bikeForNormalUserResponseDTO;
-    }
-
-    public UserForNormalUserResponseDTO getUserForNormalUserResponseDTO() {
-        return userForNormalUserResponseDTO;
-    }
-
-    public void setUserForNormalUserResponseDTO(UserForNormalUserResponseDTO userForNormalUserResponseDTO) {
-        this.userForNormalUserResponseDTO = userForNormalUserResponseDTO;
-    }
-
-    public StationForNormalUserResponseDTO getStationForNormalUserResponseDTO() {
-        return stationForNormalUserResponseDTO;
-    }
-
-    public void setStationForNormalUserResponseDTO(StationForNormalUserResponseDTO stationForNormalUserResponseDTO) {
-        this.stationForNormalUserResponseDTO = stationForNormalUserResponseDTO;
-    }
 }
--- src/main/java/com/klodnicki/Bike/v2/DTO/rent/RentResponseDTO.java
+++ src/main/java/com/klodnicki/Bike/v2/DTO/rent/RentResponseDTO.java
@@ -3,9 +3,17 @@ package com.klodnicki.Bike.v2.DTO.rent;
 import com.klodnicki.Bike.v2.DTO.bike.BikeForNormalUserResponseDTO;
 import com.klodnicki.Bike.v2.DTO.station.StationForNormalUserResponseDTO;
 import com.klodnicki.Bike.v2.DTO.user.UserForNormalUserResponseDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class RentResponseDTO {
 
     private Long id;
@@ -15,76 +23,4 @@ public class RentResponseDTO {
     private BikeForNormalUserResponseDTO bikeForNormalUserResponseDTO;
     private UserForNormalUserResponseDTO userForNormalUserResponseDTO;
     private StationForNormalUserResponseDTO stationForNormalUserResponseDTO;
-
-    public RentResponseDTO(Long id, LocalDateTime rentalStartTime, LocalDateTime rentalEndTime, int daysOfRent,
-                           BikeForNormalUserResponseDTO bikeForNormalUserResponseDTO,
-                           UserForNormalUserResponseDTO userForNormalUserResponseDTO,
-                           StationForNormalUserResponseDTO stationForNormalUserResponseDTO) {
-        this.id = id;
-        this.rentalStartTime = rentalStartTime;
-        this.rentalEndTime = rentalEndTime;
-        this.daysOfRent = daysOfRent;
-        this.bikeForNormalUserResponseDTO = bikeForNormalUserResponseDTO;
-        this.userForNormalUserResponseDTO = userForNormalUserResponseDTO;
-        this.stationForNormalUserResponseDTO = stationForNormalUserResponseDTO;
-    }
-
-    public RentResponseDTO() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getRentalStartTime() {
-        return rentalStartTime;
-    }
-
-    public void setRentalStartTime(LocalDateTime rentalStartTime) {
-        this.rentalStartTime = rentalStartTime;
-    }
-
-    public LocalDateTime getRentalEndTime() {
-        return rentalEndTime;
-    }
-
-    public void setRentalEndTime(LocalDateTime rentalEndTime) {
-        this.rentalEndTime = rentalEndTime;
-    }
-
-    public int getDaysOfRent() {
-        return daysOfRent;
-    }
-
-    public void setDaysOfRent(int daysOfRent) {
-        this.daysOfRent = daysOfRent;
-    }
-
-    public BikeForNormalUserResponseDTO getBikeForNormalUserResponseDTO() {
-        return bikeForNormalUserResponseDTO;
-    }
-
-    public void setBikeForNormalUserResponseDTO(BikeForNormalUserResponseDTO bikeForNormalUserResponseDTO) {
-        this.bikeForNormalUserResponseDTO = bikeForNormalUserResponseDTO;
-    }
-
-    public UserForNormalUserResponseDTO getUserForNormalUserResponseDTO() {
-        return userForNormalUserResponseDTO;
-    }
-
-    public void setUserForNormalUserResponseDTO(UserForNormalUserResponseDTO userForNormalUserResponseDTO) {
-        this.userForNormalUserResponseDTO = userForNormalUserResponseDTO;
-    }
-
-    public StationForNormalUserResponseDTO getStationForNormalUserResponseDTO() {
-        return stationForNormalUserResponseDTO;
-    }
-
-    public void setStationForNormalUserResponseDTO(StationForNormalUserResponseDTO stationForNormalUserResponseDTO) {
-        this.stationForNormalUserResponseDTO = stationForNormalUserResponseDTO;
-    }
 }
