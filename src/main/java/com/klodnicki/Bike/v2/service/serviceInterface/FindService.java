aa4b05075a6e490d7b1a2adb54c1f669b34d5dd2
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-25 21:27:20 +0200

    refactor of parameter in FindService interface

--- src/main/java/com/klodnicki/Bike/v2/service/serviceInterface/FindService.java
+++ src/main/java/com/klodnicki/Bike/v2/service/serviceInterface/FindService.java
@@ -2,9 +2,9 @@ package com.klodnicki.Bike.v2.service.serviceInterface;
 
 import java.util.List;
 
-public interface FindService<T> {
+public interface FindService<T, ID> {
 
-    T findById(Long id);
+    T findById(ID id);
 
     List<T> findAll();
 }
