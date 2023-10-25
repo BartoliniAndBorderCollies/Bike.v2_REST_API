1337a7b4b64ec568b1496b17d4017287a16d0aa8
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-25 21:28:24 +0200

    refactor of parameter in ReturnService interface

--- src/main/java/com/klodnicki/Bike/v2/service/serviceInterface/ReturnService.java
+++ src/main/java/com/klodnicki/Bike/v2/service/serviceInterface/ReturnService.java
@@ -1,6 +1,6 @@
 package com.klodnicki.Bike.v2.service.serviceInterface;
 
-public interface ReturnService<G, K, L, M> {
+public interface ReturnService<G, ID, ID2, ID3> {
 
-    G returnVehicle(K obj1, L obj2, M obj3);
+    G returnVehicle(ID id, ID2 id2, ID3 id3);
 }
