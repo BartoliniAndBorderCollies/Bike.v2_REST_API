20747e1f2d91e2325500b58de690d2309121b7ce
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-25 21:28:48 +0200

    add GenericUserService interface

---
+++ src/main/java/com/klodnicki/Bike/v2/service/serviceInterface/GenericUserService.java
@@ -0,0 +1,12 @@
+package com.klodnicki.Bike.v2.service.serviceInterface;
+
+import com.klodnicki.Bike.v2.DTO.user.UserForAdminResponseDTO;
+import com.klodnicki.Bike.v2.model.entity.User;
+import org.springframework.http.ResponseEntity;
+
+public interface GenericUserService extends AddService<UserForAdminResponseDTO, User>,
+        SaveService<User>, FindService<UserForAdminResponseDTO, Long>, DeleteService<Long> {
+
+    User findUserById(Long id);
+    ResponseEntity<?> banUser(Long id);
+}
