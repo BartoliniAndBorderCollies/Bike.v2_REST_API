4bc7244fc7471a423905b70443ab118a58db124e
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-26 17:28:55 +0200

    add lombok annotations to UserController

--- src/main/java/com/klodnicki/Bike/v2/rest/controller/UserController.java
+++ src/main/java/com/klodnicki/Bike/v2/rest/controller/UserController.java
@@ -2,19 +2,16 @@ package com.klodnicki.Bike.v2.rest.controller;
 
 import com.klodnicki.Bike.v2.DTO.user.UserForAdminResponseDTO;
 import com.klodnicki.Bike.v2.model.entity.User;
-import com.klodnicki.Bike.v2.service.UserService;
 import com.klodnicki.Bike.v2.service.interfacee.GenericUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
+@AllArgsConstructor
 public class UserController {
 
     private final GenericUserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @PostMapping("/add")
     public UserForAdminResponseDTO add(@RequestBody User user) {
