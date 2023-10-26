995b5a6648d4295b167de7f16164c492247f59d6
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-26 17:18:10 +0200

    refactor of field type in UserController

--- src/main/java/com/klodnicki/Bike/v2/rest/controller/UserController.java
+++ src/main/java/com/klodnicki/Bike/v2/rest/controller/UserController.java
@@ -1,16 +1,16 @@
 package com.klodnicki.Bike.v2.rest.controller;
 
 import com.klodnicki.Bike.v2.DTO.user.UserForAdminResponseDTO;
-import com.klodnicki.Bike.v2.DTO.user.UserForNormalUserResponseDTO;
 import com.klodnicki.Bike.v2.model.entity.User;
 import com.klodnicki.Bike.v2.service.UserService;
+import com.klodnicki.Bike.v2.service.interfacee.GenericUserService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-    private final UserService userService;
+    private final GenericUserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
