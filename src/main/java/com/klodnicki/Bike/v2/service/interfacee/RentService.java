9244535f5dae6ef8b20ee0af317f2b835ec35012
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-26 16:55:17 +0200

    delete of RentService and ReturnService interfaces

--- src/main/java/com/klodnicki/Bike/v2/service/interfacee/RentService.java
+++
@@ -1,5 +0,0 @@
-package com.klodnicki.Bike.v2.service.interfacee;
-
-public interface RentService<T, G> {
-    T rent(G obj);
-}
--- src/main/java/com/klodnicki/Bike/v2/service/interfacee/ReturnService.java
+++
@@ -1,6 +0,0 @@
-package com.klodnicki.Bike.v2.service.interfacee;
-
-public interface ReturnService<G, ID, ID2, ID3> {
-
-    G returnVehicle(ID id, ID2 id2, ID3 id3);
-}
