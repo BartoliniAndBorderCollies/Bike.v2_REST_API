ef2ec61351e10e20e6c39c9acf2a6b4543565fd4
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-26 16:54:57 +0200

    add GenericRentalActionService interface

---
+++ src/main/java/com/klodnicki/Bike/v2/service/interfacee/GenericRentalActionService.java
@@ -0,0 +1,9 @@
+package com.klodnicki.Bike.v2.service.interfacee;
+
+public interface GenericRentalActionService<G, H, T, ID, ID2, ID3> {
+
+    G rent(T obj);
+
+    H returnVehicle(ID id, ID2 id2, ID3 id3);
+}
+
