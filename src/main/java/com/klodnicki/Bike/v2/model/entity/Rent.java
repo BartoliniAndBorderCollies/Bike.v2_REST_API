52437c9067a8a9435fe5072ac4e53f89209f3454
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-10-27 18:03:26 +0200

    add @AllArgsConstructor to Rent class

--- src/main/java/com/klodnicki/Bike/v2/model/entity/Rent.java
+++ src/main/java/com/klodnicki/Bike/v2/model/entity/Rent.java
@@ -1,6 +1,7 @@
 package com.klodnicki.Bike.v2.model.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Rent {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,14 +32,4 @@ public class Rent {
     @OneToOne
     @JoinColumn(name = "chargingStation_id")
     private ChargingStation chargingStation;
-
-    public Rent(LocalDateTime rentalStartTime, LocalDateTime rentalEndTime, Bike bike, User user,
-                ChargingStation chargingStation, int daysOfRent) {
-        this.rentalStartTime = rentalStartTime;
-        this.rentalEndTime = rentalEndTime;
-        this.bike = bike;
-        this.user = user;
-        this.chargingStation = chargingStation;
-        this.daysOfRent = daysOfRent;
-    }
 }
