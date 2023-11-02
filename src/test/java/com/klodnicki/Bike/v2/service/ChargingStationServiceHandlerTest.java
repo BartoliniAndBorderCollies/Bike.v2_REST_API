9c8fccbe6ceaf2d91cf6456252dfb92b4c88ccbe
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-11-02 18:13:24 +0100

    add method to test if adds to database

---
+++ src/test/java/com/klodnicki/Bike/v2/service/ChargingStationServiceHandlerTest.java
@@ -0,0 +1,42 @@
+package com.klodnicki.Bike.v2.service;
+
+import com.klodnicki.Bike.v2.DTO.station.StationForAdminResponseDTO;
+import com.klodnicki.Bike.v2.model.entity.ChargingStation;
+import com.klodnicki.Bike.v2.repository.ChargingStationRepository;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ChargingStationServiceHandlerTest {
+
+    @Autowired
+    private ChargingStationRepository chargingStationRepository;
+
+    @Autowired
+    private ChargingStationServiceHandler chargingStationServiceHandler;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Test
+    public void add_ShouldAddToDatabase_WhenGivenCorrectArguments() {
+        //given
+        chargingStationRepository.deleteAll();
+        Long expected = chargingStationRepository.count() + 1;
+        ChargingStation chargingStation = new ChargingStation();
+
+        //when
+        chargingStationServiceHandler.add(chargingStation);
+        Long actual = chargingStationRepository.count();
+
+        //then
+        assertEquals(expected, actual);
+    }
+}
