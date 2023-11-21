a04fad4044b9355cc9439d5cc5a9cec7c0bdc6a7
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-11-21 11:56:09 +0100

    add FeedbackControllerIntegrationTest class and method to check addFeedback()

---
+++ src/integration_test/java/com/klodnicki/Bike/v2/rest/controller/FeedbackControllerIntegrationTest.java
@@ -0,0 +1,42 @@
+package com.klodnicki.Bike.v2.rest.controller;
+
+import com.klodnicki.Bike.v2.model.entity.Feedback;
+import com.klodnicki.Bike.v2.repository.FeedbackRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "/application-test.properties")
+class FeedbackControllerIntegrationTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+    @Autowired
+    private FeedbackRepository feedbackRepository;
+
+    @Test
+    public void addFeedback_ShouldReturnResponseEntityAsStringAndSaveFeedbackInDatabase_WhenFeedbackObjectIsGiven() {
+        Feedback feedback = new Feedback(null, "feedback test");
+        feedbackRepository.save(feedback);
+
+        webTestClient.post()
+                .uri("/api/feedback/add")
+                .bodyValue(feedback)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class)
+                .consumeWith(response -> {
+                    String stringResponse = response.getResponseBody();
+                    assertNotNull(stringResponse);
+                    assertEquals("Thanks for your feedback!", stringResponse);
+                    Feedback savedFeedback = feedbackRepository.findById(feedback.getId()).orElseThrow(IllegalArgumentException::new);
+                    assertEquals(feedback, savedFeedback);
+                });
+    }
+
+}
