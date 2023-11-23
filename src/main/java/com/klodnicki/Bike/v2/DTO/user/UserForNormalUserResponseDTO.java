6dce69c229d0cca183e69cbceab478a582b8bd64
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-11-23 08:29:58 +0100

    add @Builder to UserForNormalUserResponseDTO

--- src/main/java/com/klodnicki/Bike/v2/DTO/user/UserForNormalUserResponseDTO.java
+++ src/main/java/com/klodnicki/Bike/v2/DTO/user/UserForNormalUserResponseDTO.java
@@ -1,14 +1,12 @@
 package com.klodnicki.Bike.v2.DTO.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class UserForNormalUserResponseDTO {
 
     private Long id;
