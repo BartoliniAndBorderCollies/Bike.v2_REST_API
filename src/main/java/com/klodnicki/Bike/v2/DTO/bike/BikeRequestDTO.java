343991b7f128d93e1c2680ff7806f2113cfeeba3
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-11-26 11:37:56 +0100

    add validations to BikeRequestDTO class

--- src/main/java/com/klodnicki/Bike/v2/DTO/bike/BikeRequestDTO.java
+++ src/main/java/com/klodnicki/Bike/v2/DTO/bike/BikeRequestDTO.java
@@ -4,12 +4,12 @@ import com.klodnicki.Bike.v2.DTO.station.StationForAdminResponseDTO;
 import com.klodnicki.Bike.v2.DTO.user.UserForAdminResponseDTO;
 import com.klodnicki.Bike.v2.model.BikeType;
 import com.klodnicki.Bike.v2.model.GpsCoordinates;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -17,11 +17,17 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class BikeRequestDTO {
 
+    @NotNull(message = "Id must not be null")
     private Long id;
+    @NotBlank(message = "Serial number must not be blank")
     private String serialNumber;
+    @AssertFalse(message = "New bike cannot be already rented")
     private boolean isRented;
+    @NotNull(message = "Bike type must not be null")
     private BikeType bikeType;
+    @DecimalMin(value = "0.0", inclusive = false, message = "Amount to be paid must be greater than 0")
     private double amountToBePaid;
+    @NotNull(message = "GPS coordinates must not be null")
     private GpsCoordinates gpsCoordinates;
     private UserForAdminResponseDTO user;
     private StationForAdminResponseDTO chargingStation;
