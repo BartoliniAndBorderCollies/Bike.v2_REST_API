e0a90271f5a45bac5c73d308b84189c0e3c0cf3b
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-11-28 18:50:38 +0100

    add validations to User class

--- src/main/java/com/klodnicki/Bike/v2/model/entity/User.java
+++ src/main/java/com/klodnicki/Bike/v2/model/entity/User.java
@@ -3,6 +3,9 @@ package com.klodnicki.Bike.v2.model.entity;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,12 +23,17 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
+    @NotBlank(message = "Name must have value")
     private String name;
     private String phoneNumber;
+    @NotBlank(message = "Email must have value")
+    @Email
     private String emailAddress;
     private int accountNumber;
     private boolean isAccountValid;
+    @NotBlank(message = "Role must not be blank")
     private String role;
+    @Min(value = -50, message = "Maximum debit is 50 $")
     private double balance;
     //User is non-owning side of the relation
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
