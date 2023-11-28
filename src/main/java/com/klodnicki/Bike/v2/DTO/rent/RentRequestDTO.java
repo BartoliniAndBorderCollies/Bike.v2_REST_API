44dd96c334247774d94c16e4b3e0263516b30159
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-11-28 11:03:30 +0100

    add validations to RentRequestDTO class

--- src/main/java/com/klodnicki/Bike/v2/DTO/rent/RentRequestDTO.java
+++ src/main/java/com/klodnicki/Bike/v2/DTO/rent/RentRequestDTO.java
@@ -3,6 +3,10 @@ package com.klodnicki.Bike.v2.DTO.rent;
 import com.klodnicki.Bike.v2.DTO.bike.BikeForNormalUserResponseDTO;
 import com.klodnicki.Bike.v2.DTO.station.StationForNormalUserResponseDTO;
 import com.klodnicki.Bike.v2.DTO.user.UserForNormalUserResponseDTO;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,9 +20,13 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class RentRequestDTO {
 
+    @NotNull(message = "Id must not be null")
     private Long id;
+    @PastOrPresent(message = "Rental start time must be present or past")
     private LocalDateTime rentalStartTime;
+    @FutureOrPresent(message = "Rental end time must be present or future")
     private LocalDateTime rentalEndTime;
+    @PositiveOrZero(message = "Must be positive or zero")
     private int daysOfRent;
     private BikeForNormalUserResponseDTO bikeForNormalUserResponseDTO;
     private UserForNormalUserResponseDTO userForNormalUserResponseDTO;
