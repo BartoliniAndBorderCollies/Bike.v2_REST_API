bf43b07091f4a115046ffe23dbb98953c3cb27ad
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-11-29 21:08:38 +0100

    add CustomUserDetailsService class with implementation of UserDetailsService

---
+++ src/main/java/com/klodnicki/Bike/v2/service/CustomUserDetailsService.java
@@ -0,0 +1,19 @@
+package com.klodnicki.Bike.v2.service;
+
+import com.klodnicki.Bike.v2.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmailAddress(email).orElseThrow(() -> new UsernameNotFoundException("Username with " + email + " was not found!"));
+        //consumer, supplier, function, predicate
+    }
+}
