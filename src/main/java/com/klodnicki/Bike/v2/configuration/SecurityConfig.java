2bd08089c7158697eefed80165109a1efca4bb31
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-12-01 10:28:19 +0100

    add httpBasic to SecurityConfig to be able to sent requests using BasicAuth

--- src/main/java/com/klodnicki/Bike/v2/configuration/SecurityConfig.java
+++ src/main/java/com/klodnicki/Bike/v2/configuration/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -33,6 +34,7 @@ public class SecurityConfig {
                 .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)) //ta linia umozliwia dostep do bazy danych h2 console
                 .formLogin(Customizer.withDefaults())
 //                .logout(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
                 .build();
     }
 
