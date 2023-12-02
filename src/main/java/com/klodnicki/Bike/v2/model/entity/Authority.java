5a27e0ba8804ad710c3b48500122a225e53f21bc
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-12-02 14:13:35 +0100

    add Authority class

---
+++ src/main/java/com/klodnicki/Bike/v2/model/entity/Authority.java
@@ -0,0 +1,26 @@
+package com.klodnicki.Bike.v2.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Authority implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String name;
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
+}
