63ab7bd9966a5b88a17a1777751088986fc27108
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-12-03 12:45:29 +0100

    add equals() hashcode() to be able to compare two iterables

--- src/main/java/com/klodnicki/Bike/v2/model/entity/Authority.java
+++ src/main/java/com/klodnicki/Bike/v2/model/entity/Authority.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 
+import java.util.Objects;
+
 @Entity
 @Getter
 @Setter
@@ -22,5 +24,18 @@ public class Authority implements GrantedAuthority {
     @Override
     public String getAuthority() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Authority authority = (Authority) o;
+        return Objects.equals(id, authority.id) && Objects.equals(name, authority.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 }
