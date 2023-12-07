1aa8db014340500ab874e55737e7a27da9f89e7d
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-12-07 09:47:15 +0100

    add NotFoundInDbException handler method

--- src/main/java/com/klodnicki/Bike/v2/rest/controller/advice/GlobalCustomExceptionHandler.java
+++ src/main/java/com/klodnicki/Bike/v2/rest/controller/advice/GlobalCustomExceptionHandler.java
@@ -1,11 +1,11 @@
 package com.klodnicki.Bike.v2.rest.controller.advice;
 
+import com.klodnicki.Bike.v2.exception.NotFoundInDatabaseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
@@ -19,5 +19,10 @@ public class CustomExceptionHandler {
         }
         return new ResponseEntity<>("Your request miss required data, check your request with validation requirements",
                 HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NotFoundInDatabaseException.class)
+    public ResponseEntity<?> handleNotFoundInDatabaseException(NotFoundInDatabaseException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
