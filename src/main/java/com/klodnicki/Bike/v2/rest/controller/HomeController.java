cd9a3313bd3df6bcc13d14625a2f4a8f34dca32c
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-12-08 16:44:45 +0100

    add JavaDoc to HomeController class

--- src/main/java/com/klodnicki/Bike/v2/rest/controller/HomeController.java
+++ src/main/java/com/klodnicki/Bike/v2/rest/controller/HomeController.java
@@ -3,6 +3,10 @@ package com.klodnicki.Bike.v2.rest.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @version 1.0
+ * @author Bartłomiej Kłodnicki
+ */
 @RestController
 @RequestMapping("/api/v1")
 public class HomeController {
