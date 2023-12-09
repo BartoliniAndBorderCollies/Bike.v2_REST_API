a00bf25ba52a61eb28bf42f0785d2660529be1e9
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-12-09 10:22:54 +0100

    add JavaDoc to FeedbackServiceHandler class

--- src/main/java/com/klodnicki/Bike/v2/service/FeedbackServiceHandler.java
+++ src/main/java/com/klodnicki/Bike/v2/service/FeedbackServiceHandler.java
@@ -14,6 +14,13 @@ public class FeedbackServiceHandler implements FeedbackServiceApi {
 
     private final FeedbackRepository feedbackRepository;
 
+    /**
+     * This method is used to add a new {@link Feedback}.
+     * It saves the feedback object in the repository and returns a ResponseEntity with a thank you message and HTTP status OK.
+     *
+     * @param feedback The feedback to be added.
+     * @return ResponseEntity A response entity containing a thank you message and HTTP status OK.
+     */
     @Override
     public ResponseEntity<?> add(Feedback feedback) {
         feedbackRepository.save(feedback);
