d4f4be24c6e83e9a8bc6fdddcebd54e313b50e5f
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-12-10 12:49:43 +0100

    add JavaDoc service interfaces

--- src/main/java/com/klodnicki/Bike/v2/service/api/ChargingStationServiceApi.java
+++ src/main/java/com/klodnicki/Bike/v2/service/api/ChargingStationServiceApi.java
@@ -7,9 +7,19 @@ import com.klodnicki.Bike.v2.service.api.operation.AddOperation;
 import com.klodnicki.Bike.v2.service.api.operation.FindOperation;
 import com.klodnicki.Bike.v2.service.api.operation.SaveOperation;
 
+/**
+ * This interface defines the operations for the ChargingStationService.
+ * It extends AddOperation, FindOperation, and SaveOperation interfaces.
+ */
 public interface ChargingStationServiceApi extends AddOperation<StationForAdminResponseDTO, ChargingStation>,
         FindOperation<StationForAdminResponseDTO, Long>, SaveOperation<ChargingStation> {
     ChargingStation findStationById(Long id) throws NotFoundInDatabaseException;
 
+    /**
+     * This method is used to add a bike to a charging station's list.
+     * @param chargingStationId This is the ID of the charging station where the bike will be added.
+     * @param bikeId This is the ID of the bike to be added to the charging station's list.
+     * @return ChargingStation This returns the updated charging station object after the bike has been added.
+     */
     ChargingStation addBikeToList(Long chargingStationId, Long bikeId) throws NotFoundInDatabaseException;
 }
--- src/main/java/com/klodnicki/Bike/v2/service/api/RentBikeServiceApi.java
+++ src/main/java/com/klodnicki/Bike/v2/service/api/RentBikeServiceApi.java
@@ -9,10 +9,20 @@ import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
+/**
+ * This interface defines the operations for the RentBikeService.
+ * It extends the RentServiceApi interface.
+ */
 public interface RentBikeServiceApi extends RentServiceApi<RentResponseDTO, ResponseEntity<?>, RentRequest,
         Long, Long> {
 
     List<BikeForNormalUserResponseDTO> findAvailableBikes();
     BikeForNormalUserResponseDTO findBikeForNormalUserById(Long id) throws NotFoundInDatabaseException;
+    /**
+     * This method is used to update a rent.
+     * @param id This is the ID of the rent to be updated.
+     * @param rentRequestDTO This is a request object which contains the updated details of the rent.
+     * @return RentResponseDTO This returns the response object with details of the updated rent.
+     */
     RentResponseDTO updateRent(Long id, RentRequestDTO rentRequestDTO) throws NotFoundInDatabaseException;
 }
