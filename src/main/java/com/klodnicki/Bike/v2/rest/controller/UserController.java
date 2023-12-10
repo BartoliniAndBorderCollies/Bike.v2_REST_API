17172996ae431bf3fcccbaf188e5d73d09da9e26
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-12-10 12:39:55 +0100

    add JavaDoc To UserController class

--- src/main/java/com/klodnicki/Bike/v2/rest/controller/UserController.java
+++ src/main/java/com/klodnicki/Bike/v2/rest/controller/UserController.java
@@ -15,6 +15,16 @@ public class UserController {
 
     private final UserServiceApi userService;
 
+    /**
+     * This method is used to add a new user.
+     * It takes a User object as input and returns a UserForAdminResponseDTO object.
+     * User must fulfil some requirements since it is preceded by @Valid.
+     *
+     * @param user This is a request object which contains the details of the user to be added.
+     * @return UserForAdminResponseDTO This returns the response object with details of the added user.
+     *
+     * @PostMapping("/add")
+     */
     @PostMapping("/add")
     public UserForAdminResponseDTO add(@Valid @RequestBody User user) {
         return userService.add(user);
