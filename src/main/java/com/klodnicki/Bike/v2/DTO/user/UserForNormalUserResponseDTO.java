e4a0a23132479abc0e35213c31422087c73f5714
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-12-11 12:11:07 +0100

    replace of type from Authority to AuthorityDTO in UserForNormalUserResponseDTO

--- src/main/java/com/klodnicki/Bike/v2/DTO/user/UserForNormalUserResponseDTO.java
+++ src/main/java/com/klodnicki/Bike/v2/DTO/user/UserForNormalUserResponseDTO.java
@@ -1,5 +1,6 @@
 package com.klodnicki.Bike.v2.DTO.user;
 
+import com.klodnicki.Bike.v2.DTO.authority.AuthorityDTO;
 import com.klodnicki.Bike.v2.model.entity.Authority;
 import lombok.*;
 
@@ -15,6 +16,5 @@ public class UserForNormalUserResponseDTO {
     private Long id;
     private String name;
     private boolean isAccountValid;
-    private Set<Authority> authorities;
-
+    private Set<AuthorityDTO> authorities;
 }
