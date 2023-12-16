d7dea9d741572c91a2ea321a691065beccbdd15e
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-12-16 10:22:01 +0100

    add javaDoc to describe operation interfaces

--- src/main/java/com/klodnicki/Bike/v2/service/api/operation/AddOperation.java
+++ src/main/java/com/klodnicki/Bike/v2/service/api/operation/AddOperation.java
@@ -1,5 +1,11 @@
 package com.klodnicki.Bike.v2.service.api.operation;
 
+/**
+ * Interface for an add operation.
+ *
+ * @param <T> the type of object that this interface returns.
+ * @param <G> the type of object that this interface accepts.
+ */
 public interface AddOperation<T, G> {
 
     T add(G obj);
--- src/main/java/com/klodnicki/Bike/v2/service/api/operation/DeleteOperation.java
+++ src/main/java/com/klodnicki/Bike/v2/service/api/operation/DeleteOperation.java
@@ -1,5 +1,10 @@
 package com.klodnicki.Bike.v2.service.api.operation;
 
+/**
+ * Interface for a delete operation.
+ *
+ * @param <ID> the type of identifier that this interface accepts.
+ */
 public interface DeleteOperation<ID> {
 
     void deleteById(ID id);
