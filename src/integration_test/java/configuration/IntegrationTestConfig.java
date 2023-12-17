79aa3ff04851cf06fc853c878bb885efeb331a98
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-12-17 10:15:30 +0100

    add javaDoc to IntegrationTestConfig class

--- src/integration_test/java/configuration/IntegrationTestConfig.java
+++ src/integration_test/java/configuration/IntegrationTestConfig.java
@@ -5,9 +5,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
+/**
+ * This class provides configuration for integration tests.
+ * It is marked with @TestConfiguration to indicate that it should not be picked up by scanning.
+ */
 @TestConfiguration
 public class IntegrationTestConfig {
 
+    /**
+     * This method provides a bean of MvcRequestMatcher.Builder.
+     * MvcRequestMatcher.Builder is used to build request matchers for Spring MVC.
+     *
+     * @param introspector The HandlerMappingIntrospector to be used by the MvcRequestMatcher.Builder.
+     * @return MvcRequestMatcher.Builder A builder for MvcRequestMatcher.
+     */
     @Bean
     MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
         return new MvcRequestMatcher.Builder(introspector);
