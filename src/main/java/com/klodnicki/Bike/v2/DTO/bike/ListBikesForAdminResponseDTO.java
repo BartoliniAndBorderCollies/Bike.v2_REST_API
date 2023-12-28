543c4f541576d56fbcbf59ac5947772018e1ed7e
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-12-28 14:16:09 +0100

    refactor of JavaDoc to ListBikesForAdminResponseDTO

--- src/main/java/com/klodnicki/Bike/v2/DTO/bike/ListBikesForAdminResponseDTO.java
+++ src/main/java/com/klodnicki/Bike/v2/DTO/bike/ListBikesForAdminResponseDTO.java
@@ -8,12 +8,14 @@ import lombok.Setter;
 import java.util.List;
 
 /**
- * Data Transfer Object (DTO) for a list of Bike information in the context of Admin operations. This was created
- * to hold the list of Bike DTOs in one object. It can be used both: as response and request. The content of the information is intended for Admin.
- * The class is annotated with @Getter, @Setter, @AllArgsConstructor, and @NoArgsConstructor
- * from the Lombok library to automatically generate boilerplate code like getters, setters,
- * and constructors.
+ * This class is used to hold the list of {@link BikeForAdminResponseDTO} in one object. It can be used both as response and request in REST API.
+ * The content of the information kept in this object is dedicated for users with ROLE_ADMIN only.
+ * <br>
+ * This class has annotations from the Lombok library to automatically generate boilerplate code like getters, setters,
+ * equals, hashcode, and toString methods,
+ * a builder pattern, and constructors with no arguments and all arguments.
  */
+
 @Getter
 @Setter
 @AllArgsConstructor
